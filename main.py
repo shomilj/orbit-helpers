111cbfcbde0201101aa599c7cc387d33f01123fe
Shomil Jain <shomil@berkeley.edu>
2021-03-28 11:49:46 -0700

    updated helper functions

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import json
 from sources.reddit import fetch_reddit
 from sources.calendar import fetch_calendar
 from sources.rss import fetch_articles
+from sources.dininghall import fetch_dining
 
+# gcloud config set project orbit-000
 # gcloud functions deploy orbit_api --runtime python38 --trigger-http --allow-unauthenticated
 
 
@@ -17,8 +19,12 @@ def orbit_api(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    if type(request) is not dict:
+        request_json = request.get_json(silent=True)
+        request_args = request.args
+    else:
+        request_json = request.get('json')
+        request_args = request.get('args')
 
     if request_json and 'source' in request_json:
         source = request_json['source']
@@ -37,5 +43,7 @@ def orbit_api(request):
         return fetch_articles(url=request['url'], source='dailycal')
     elif source == 'berkeleyside':
         return fetch_articles(url=request['url'], source='berkeleyside')
+    elif source == 'dining':
+        return fetch_dining()
     else:
         return json.dumps({'error': 'Unknown route.'})
